21a4297d85dbf531fdf50f31a70c8b2f6912e9f2
NicolaTonelli <nicola.tonelli@studenti.isissgobetti.it>
2024-05-23 11:42:08 +0200

    aggiunto markdown es 7

--- 1_json_dizionari/esercizio_004.py
+++ 1_json_dizionari/esercizio_004.py
@@ -9,21 +9,22 @@ N.B
 
 import json
 
-# Apro il file JSON e carico il dizionario
-with open('es004.json', 'r') as file:
-    data = json.load(file)
+def main():
+    # Apro il file JSON e carico il dizionario
+    with open('es004.json', 'r') as file:
+        data = json.load(file)
 
-# Stampo le chiavi
-print("Chiavi:")
-for key in data.keys():
-    print(key)
+    # Stampo le chiavi
+    print("Chiavi:")
+    for key in data.keys():
+        print(key)
 
-# Stampo i valori
-print("\nValori:")
-for value in data.values():
-    print(value)
+    # Stampo i valori
+    print("\nValori:")
+    for value in data.values():
+        print(value)
 
-# Stampo le coppie chiave-valore
-print("\nChiavi-Valori:")
-for key, value in data.items():
-    print(f"{key}: {value}")
+    # Stampo le coppie chiave-valore
+    print("\nChiavi-Valori:")
+    for key, value in data.items():
+        print(f"{key}: {value}")
--- 1_json_dizionari/esercizio_005.py
+++ 1_json_dizionari/esercizio_005.py
@@ -17,89 +17,90 @@ NB
 
 import json
 
-# 1) Creare una nuova stanza
-def crea_stanza(museo: dict):
-    # input
-    nome_stanza = input("Inserisci il nome della stanza: ")
-    # elaborazione
-    stanza = {
-        "nome": nome_stanza,
-        "opere": [],
-    }
-    # controllo se la stanza è già presente
-    for stanza in museo["stanze"]:
-        if stanza["nome"] == nome_stanza:
-            print("La stanza è già presente!")
-            return None
-    museo["stanze"].append(stanza)
-    # output
-    print("Stanza creata con successo!")
-    return stanza
+def main():
+    # 1) Creare una nuova stanza
+    def crea_stanza(museo: dict):
+        # input
+        nome_stanza = input("Inserisci il nome della stanza: ")
+        # elaborazione
+        stanza = {
+            "nome": nome_stanza,
+            "opere": [],
+        }
+        # controllo se la stanza è già presente
+        for stanza in museo["stanze"]:
+            if stanza["nome"] == nome_stanza:
+                print("La stanza è già presente!")
+                return None
+        museo["stanze"].append(stanza)
+        # output
+        print("Stanza creata con successo!")
+        return stanza
 
-# 2) Aggiungere un opera ad una stanza(titolo, artista, anno)
-def aggiungi_opera(museo: dict):
-    # input
-    nome_stanza = input("Inserisci il nome della stanza: ")
-    titolo_opera = input("Inserisci il titolo dell'opera: ")
-    artista_opera = input("Inserisci l'artista dell'opera: ")
-    anno_opera = int(input("Inserisci l'anno dell'opera: "))
-    # elaborazione
-    opera = {
-        "titolo": titolo_opera,
-        "artista": artista_opera,
-        "anno": anno_opera,
-    }
-    # controllo se la stanza è già presente
-    for stanza in museo["stanze"]:
-        if stanza["nome"] == nome_stanza:
-            # controllo se l'opera è già presente nella stanza
-            # TODO meglio controllare se l'opera è univoca in tutto il museo
-            for opera in stanza["opere"]:
-                if opera["titolo"] == titolo_opera:
-                    print("Opera già presente!")
-                    return None
-            stanza["opere"].append(opera)
-            print("Opera aggiunta con successo!")
-            return opera
-    print("Stanza non presente!")
-    return None
-
-# 3) Consultare le opere presenti in una stanza
-def cerca_stanza(museo: dict, nome_stanza: str) -> dict:
-    # elaborazione
-    for stanza in museo["stanze"]:
-        if stanza["nome"] == nome_stanza:
-            return stanza
-    return None
-
-def stampa_opere_stanza(stanza: dict) -> None:
-    # output
-    if stanza is not None:
-        print("Opere presenti nella stanza:")
-        for opera in stanza["opere"]:
-            print(opera["titolo"])
-    else:
+    # 2) Aggiungere un opera ad una stanza(titolo, artista, anno)
+    def aggiungi_opera(museo: dict):
+        # input
+        nome_stanza = input("Inserisci il nome della stanza: ")
+        titolo_opera = input("Inserisci il titolo dell'opera: ")
+        artista_opera = input("Inserisci l'artista dell'opera: ")
+        anno_opera = int(input("Inserisci l'anno dell'opera: "))
+        # elaborazione
+        opera = {
+            "titolo": titolo_opera,
+            "artista": artista_opera,
+            "anno": anno_opera,
+        }
+        # controllo se la stanza è già presente
+        for stanza in museo["stanze"]:
+            if stanza["nome"] == nome_stanza:
+                # controllo se l'opera è già presente nella stanza
+                # TODO meglio controllare se l'opera è univoca in tutto il museo
+                for opera in stanza["opere"]:
+                    if opera["titolo"] == titolo_opera:
+                        print("Opera già presente!")
+                        return None
+                stanza["opere"].append(opera)
+                print("Opera aggiunta con successo!")
+                return opera
         print("Stanza non presente!")
+        return None
 
-museo = {
-    "nome": "Museo di Roma",
-    "stanze": [],
-}
+    # 3) Consultare le opere presenti in una stanza
+    def cerca_stanza(museo: dict, nome_stanza: str) -> dict:
+        # elaborazione
+        for stanza in museo["stanze"]:
+            if stanza["nome"] == nome_stanza:
+                return stanza
+        return None
 
-museo["stanze"].append(
-    {"nome": "Rinascimento", "opere": 
-    [{"titolo": "La creazione di Adamo", "artista": "Michelangelo", "anno": 1512}]
+    def stampa_opere_stanza(stanza: dict) -> None:
+        # output
+        if stanza is not None:
+            print("Opere presenti nella stanza:")
+            for opera in stanza["opere"]:
+                print(opera["titolo"])
+        else:
+            print("Stanza non presente!")
+
+    museo = {
+        "nome": "Museo di Roma",
+        "stanze": [],
     }
-    )
-# crea_stanza(museo)
-# aggiungi_opera(museo)
 
-# stampa opere della stanza
-nome_stanza = "dfsdfsdfsdfs"
-stanza = cerca_stanza(museo, nome_stanza)
-stampa_opere_stanza(stanza)
+    museo["stanze"].append(
+        {"nome": "Rinascimento", "opere": 
+        [{"titolo": "La creazione di Adamo", "artista": "Michelangelo", "anno": 1512}]
+        }
+        )
+    # crea_stanza(museo)
+    # aggiungi_opera(museo)
 
-# stampa opere della stanza
-nome_stanza = "Rinascimento"
-stanza = cerca_stanza(museo, nome_stanza)
-stampa_opere_stanza(stanza)
+    # stampa opere della stanza
+    nome_stanza = "dfsdfsdfsdfs"
+    stanza = cerca_stanza(museo, nome_stanza)
+    stampa_opere_stanza(stanza)
+
+    # stampa opere della stanza
+    nome_stanza = "Rinascimento"
+    stanza = cerca_stanza(museo, nome_stanza)
+    stampa_opere_stanza(stanza)
